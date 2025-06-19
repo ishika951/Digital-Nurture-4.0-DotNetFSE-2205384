1328b3b864bb1bd27f1aaf57e3d98300fcdc601c
ishika951 <ishikadubey80@gmail.com>
2025-06-19 11:15:45 +0530

    Update exercise2.java

--- solutions-dotnet_fse/week-1/exercise2/exercise2.java
+++ solutions-dotnet_fse/week-1/exercise2/exercise2.java
@@ -1,1 +1,45 @@
+public interface Shape {
+    void draw();
+}
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle!");
+    }
+}
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Rectangle!");
+    }
+}
+
+public class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+        }
+
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        }
+
+        return null;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        shape1.draw();  // Output: Drawing a Circle!
+
+        Shape shape2 = shapeFactory.getShape("RECTANGLE");
+        shape2.draw();  // Output: Drawing a Rectangle!
+    }
+}
+
 
