788595544e6fde0e131eb30ec25e7837a5500d1e
ishika951 <ishikadubey80@gmail.com>
2025-06-23 10:05:01 +0530

    Update exercise1.cs

--- solutions-dotnet_fse/week-1/exercise1/exercise1.cs
+++ solutions-dotnet_fse/week-1/exercise1/exercise1.cs
@@ -1,1 +1,24 @@
+class Singleton
+{
+    // Static instance
+    public static Singleton instance = new Singleton();
 
+    // Private constructor
+    private Singleton()
+    {
+        Console.WriteLine("Singleton Created.");
+    }
+
+    public void HelloWorld()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+class exercise1
+{
+    static void Main(string[] args)
+    {
+        Singleton.instance.HelloWorld();
+    }
+}
